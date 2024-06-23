86088c379f8b567bd282ff5427c0e9b73a548ad6
Mohamed <msaad@goatar.com>
2024-06-24 02:03:29 +0300

    Add graphql and configure it

--- back-end/src/app.module.ts
+++ back-end/src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { GraphQLModule } from '@nestjs/graphql';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { join } from 'path';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      typePaths: ['./**/*.graphql'],
+      definitions: {
+        path: join(process.cwd(), 'src/graphql.ts'),
+        outputAs: 'class',
+      },
+    }),
+    UserModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ back-end/src/graphql.ts
@@ -0,0 +1,90 @@
+
+/*
+ * -------------------------------------------------------
+ * THIS FILE WAS AUTOMATICALLY GENERATED (DO NOT MODIFY)
+ * -------------------------------------------------------
+ */
+
+/* tslint:disable */
+/* eslint-disable */
+
+export class LocalizedNameInput {
+    firstName: string;
+    fatherName: string;
+    grandfatherName: string;
+    familyName: string;
+}
+
+export class NationalIdInput {
+    idNumber: string;
+    expiryDate: string;
+}
+
+export class NationalityInput {
+    countryId: number;
+}
+
+export class MaritalStatusInput {
+    id: number;
+}
+
+export class UpdateUserInput {
+    firstName: string;
+    fatherName: string;
+    grandfatherName: string;
+    familyName: string;
+    localizedName?: Nullable<LocalizedNameInput>;
+    nationalId?: Nullable<NationalIdInput>;
+    nationalities?: Nullable<NationalityInput[]>;
+    maritalStatus?: Nullable<MaritalStatusInput>;
+    dependants: number;
+}
+
+export class LocalizedName {
+    firstName: string;
+    fatherName: string;
+    grandfatherName: string;
+    familyName: string;
+}
+
+export class NationalId {
+    idNumber: string;
+    expiryDate: string;
+}
+
+export class Country {
+    id: number;
+    name: string;
+}
+
+export class Nationality {
+    country: Country;
+    countryId: number;
+}
+
+export class MaritalStatus {
+    id: number;
+    name: string;
+}
+
+export class User {
+    firstName: string;
+    fatherName: string;
+    grandfatherName: string;
+    familyName: string;
+    localizedName?: Nullable<LocalizedName>;
+    nationalId?: Nullable<NationalId>;
+    nationalities?: Nullable<Nationality[]>;
+    maritalStatus?: Nullable<MaritalStatus>;
+    dependants: number;
+}
+
+export abstract class IQuery {
+    abstract user(id: number): Nullable<User> | Promise<Nullable<User>>;
+}
+
+export abstract class IMutation {
+    abstract updateUser(id: number, input: UpdateUserInput): Nullable<User> | Promise<Nullable<User>>;
+}
+
+type Nullable<T> = T | null;
